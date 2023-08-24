19ec9eb27cd6bc970e73424f3a2d092db22aa895
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-08-24 11:25:39 +0900

    Janken アイコンを追加

--- lib/main.dart
+++ lib/main.dart
@@ -11,113 +11,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Column(
-            children: [
-              const Text(
-                'eunsol',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'ツイート 62,689',
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.8),
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-            ],
-          ),
-        ),
-        body: const SingleChildScrollView(
-          child: Column(
-            children: [
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-              TweetTile(),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: JanKenPage(),
     );
   }
 }
 
-class TweetTile extends StatelessWidget {
-  const TweetTile({
-    super.key,
-  });
+class JanKenPage extends StatefulWidget {
+  const JanKenPage({super.key});
 
   @override
+  State<JanKenPage> createState() => _JanKenPageState();
+}
+
+class _JanKenPageState extends State<JanKenPage> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CircleAvatar(
-            backgroundColor: Colors.transparent,
-            backgroundImage: NetworkImage(
-              'https://github.com/sashapivovarova/memo/blob/main/eunsol.png?raw=true',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Janken'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('✊'),
             ),
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Row(
-                children: [
-                  Text('eunsol'),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text('2023 / 08 / 24'),
-                ],
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              const Text('Hello'),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.favorite_border_rounded,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.repeat_rounded,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('✌️'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('🖐'),
+            ),
+          ],
+        ),
       ),
     );
   }
