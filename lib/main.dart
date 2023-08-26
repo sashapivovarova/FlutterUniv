f908bd0fde3da46fea9b8efc57f2968c52987407
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-08-26 13:07:20 +0900

    appBar に検索ボックスを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'api_key.dart';
 
 void main() {
   runApp(
@@ -26,8 +28,51 @@ class PixabayPage extends StatefulWidget {
 }
 
 class _PixabayPageState extends State<PixabayPage> {
+  List hits = [];
+
+  Future<void> fetchImages(String text) async {
+    String path =
+        'https://pixabay.com/api/?key=$apiKey=$text&image_type=photo&per_page=100';
+    Response response = await Dio().get(path);
+    hits = response.data['hits'];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImages('flower');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          initialValue: 'flower',
+          decoration: const InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (value) {
+            fetchImages(value);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: hits.length,
+          itemBuilder: (context, index) {
+            Map<String, dynamic> hit = hits[index];
+            return Image.network(
+              hit['previewURL'],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
