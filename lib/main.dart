bdf3c26eaf0a53f0a97226f79d90ca203c117749
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-08-28 11:42:46 +0900

    Firebase を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:sample/objectbox.g.dart';
-import 'package:sample/model/life_event.dart';
 
-void main() {
-  runApp(
-    const MyApp(),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.ios,
   );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,101 +15,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LifeCounterPage(),
-    );
-  }
-}
-
-class LifeCounterPage extends StatefulWidget {
-  const LifeCounterPage({super.key});
-
-  @override
-  State<LifeCounterPage> createState() => _LifeCounterPageState();
-}
-
-class _LifeCounterPageState extends State<LifeCounterPage> {
-  Store? store;
-  Box<LifeEvent>? lifeEventBox;
-  List<LifeEvent> lifeEvents = [];
-
-  @override
-  void initState() {
-    super.initState();
-    initialize();
-  }
-
-  Future<void> initialize() async {
-    store = await openStore();
-    lifeEventBox = store?.box<LifeEvent>();
-    fetchLifeEvents();
-  }
-
-  void fetchLifeEvents() {
-    lifeEvents = lifeEventBox?.getAll() ?? [];
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Life Counter'),
-      ),
-      body: ListView.builder(
-        itemCount: lifeEvents.length,
-        itemBuilder: (context, index) {
-          final lifeEvent = lifeEvents[index];
-          return Text(lifeEvent.title);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(
-          Icons.add,
-          size: 40,
-        ),
-        onPressed: () async {
-          final newLifeEvent = await Navigator.of(context).push<LifeEvent>(
-            MaterialPageRoute(
-              builder: (context) {
-                return const AddLifeEventPage();
-              },
-            ),
-          );
-          if (newLifeEvent != null) {
-            lifeEventBox?.put(newLifeEvent);
-            fetchLifeEvents();
-          }
-        },
-      ),
-    );
-  }
-}
-
-class AddLifeEventPage extends StatefulWidget {
-  const AddLifeEventPage({super.key});
-
-  @override
-  State<AddLifeEventPage> createState() => _AddLifeEventPageState();
-}
-
-class _AddLifeEventPageState extends State<AddLifeEventPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Life Event'),
-      ),
-      body: TextFormField(
-        onFieldSubmitted: (text) {
-          final lifeEvent = LifeEvent(title: text, count: 0);
-          Navigator.of(context).pop(lifeEvent);
-        },
-      ),
     );
   }
 }
